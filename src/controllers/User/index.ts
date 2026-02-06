357b5c60bf8cefa65edd2b27baa1c6a887b4cc21
Rafaelgbi <rafa-ibg@outlook.com>
2026-02-05 22:49:01 -0300

    feat:  criando novas logicas ao swagger e criando u getByEmail

--- src/controllers/User/index.ts
+++ src/controllers/User/index.ts
@@ -1,5 +1,7 @@
 import * as creatUser from './create.js';
+import * as getUserEmail from './getEmail.js';
 
 export const UserController = {
     ...creatUser,
+    ...getUserEmail,
 }
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,37 @@
-import express from 'express';
-import { router } from './routes.js';
-import sweggerUi from 'swagger-ui-express';
-import swagggerDocs from './swagger.json' with { type: "json" };;
+import express from "express";
+import { router } from "./routes.js";
+import swaggerUi from "swagger-ui-express";
+import SwaggerParser from "@apidevtools/swagger-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = express();
 const port = 3000;
 
 server.use(express.json());
 
-server.use('/api-docs', sweggerUi.serve, sweggerUi.setup(swagggerDocs))
+async function setupSwagger() {
+  const swaggerPath = path.join(__dirname, "docs", "swagger.json");
 
-//chamando as routes
+  // 🔥 Aqui ele junta todos os arquivos referenciados
+  const api = await SwaggerParser.bundle(swaggerPath);
+
+  server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(api));
+}
+
+await setupSwagger();
+
+// suas rotas
 server.use(router);
 
-//testando
-server.get('/test', (req, res)=>{
-    return res.status(200).json({msg:'Rota de test, OK!'})
+server.get("/test", (req, res) => {
+  return res.status(200).json({ msg: "Rota de teste OK!" });
 });
 
-server.listen(port, ()=>{
-    console.log(`Servidor rodando na porta ${port}`);
+server.listen(port, () => {
+  console.log(`🚀 Servidor rodando na porta ${port}`);
+  console.log(`📘 Swagger em http://localhost:${port}/api-docs`);
 });
